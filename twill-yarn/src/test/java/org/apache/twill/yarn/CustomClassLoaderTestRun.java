74ca3188d85ffa000b96a6f073bc12918b1ef045
DrRacket <zpy1995@gmail.com>
2017-11-27 07:24:57 +0000

    cc79f0d0b23c replay starts

--- twill-yarn/src/test/java/org/apache/twill/yarn/CustomClassLoaderTestRun.java
+++ twill-yarn/src/test/java/org/apache/twill/yarn/CustomClassLoaderTestRun.java
@@ -34,11 +34,9 @@ public class CustomClassLoaderTestRun extends BaseYarnTest {
     TwillController controller = getTwillRunner().prepare(new CustomClassLoaderRunnable())
       .setClassLoader(CustomClassLoader.class.getName())
       .addLogHandler(new PrinterLogHandler(new PrintWriter(System.out, true)))
-      .setJVMOptions("-Dservice.port=54321")
-      .setJVMOptions(CustomClassLoaderRunnable.class.getSimpleName(), "-Dservice.name=custom")
       .start();
 
-    Assert.assertTrue(waitForSize(controller.discoverService("custom"), 1, 120));
+    Assert.assertTrue(waitForSize(controller.discoverService(CustomClassLoaderRunnable.SERVICE_NAME), 1, 120));
     controller.terminate().get();
   }
 }
